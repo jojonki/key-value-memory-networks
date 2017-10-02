e0b304dfbe9fcb69a4ed23e91b4bb8274b371ddc
jojonki <junki.ohmura@gmail.com>
2017-10-02 15:11:05 -0400

    use pickle

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from itertools import chain
 from sklearn import model_selection, metrics
 import pprint
 
-from process_data import load_task, vectorize
+from process_data import load_task, vectorize, save_pickle, load_pickle
 from net.memn2n import MemN2N
 from net.memn2n_kv import MemN2N_KV, add_gradient_noise
 
@@ -44,32 +44,52 @@ tf.flags.DEFINE_string("checkpoint_dir", "checkpoints", "checkpoint directory [c
 FLAGS = tf.flags.FLAGS
 
 def main(_):
+    is_babi = False
+    is_load_pickle = False
 
-    train_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_train.txt')
-    test_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_test.txt')
+    if is_load_pickle:
+        train_data = load_pickle('train_data.pickle')[:100]
+        test_data = load_pickle('test_data.pickle')[:100]
+    else:
+        if is_babi:
+            train_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_train.txt')
+            test_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_test.txt')
+        else: # movie qa
+            train_data = load_task('./data/movie_dialog_dataset/task1_qa/task1_qa_train.txt')
+            test_data = load_task('./data/movie_dialog_dataset/task1_qa/task1_qa_test.txt')
+
     data = train_data + test_data
+    data = data
 
-    vocab = functools.reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data))
-    w2i = dict((c, i) for i, c in enumerate(vocab, 1))
-    i2w = dict((i, c) for i, c in enumerate(vocab, 1))
+    if is_load_pickle:
+        vocab = load_pickle('vocab.pickle')
+        w2i = load_pickle('w2i.pickle')
+        i2w = load_pickle('i2w.pickle')
+    else:
+        vocab = functools.reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data))
+        w2i = dict((c, i) for i, c in enumerate(vocab, 1))
+        i2w = dict((i, c) for i, c in enumerate(vocab, 1))
 
-    memory_size = 10 # TBD
-    max_story_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
-    sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
+    max_story_size = max(map(len, (s for s, _, _ in data)))
+    len_list = list(map(len, chain.from_iterable(s for s, _, _ in data)))
+    if len(len_list) != 0:
+        sentence_size = max(len_list)
+    else:
+        sentence_size = 0
     question_size = max(map(len, chain.from_iterable(q for _, q, _ in data)))
     max_sentence_size = max(sentence_size, question_size)
     vocab_size = len(w2i) + 1 # +1 for nil word
 
     FLAGS.story_size = max_sentence_size
     FLAGS.query_size = max_sentence_size
-    FLAGS.memory_key_size = memory_size
-    FLAGS.memory_value_size = memory_size
+    FLAGS.memory_key_size = FLAGS.mem_size
+    FLAGS.memory_value_size = FLAGS.mem_size
     FLAGS.vocab_size = vocab_size
     print('max_story_size={}\nmax_sentence_size={}\nvocab_size={}'.format(max_story_size, max_sentence_size, vocab_size))
 
-    S, Q, A = vectorize(data, w2i, max_sentence_size, memory_size)
+    S, Q, A = vectorize(data, w2i, max_sentence_size, FLAGS.mem_size)
     trainS, valS, trainQ, valQ, trainA, valA = model_selection.train_test_split(S, Q, A, test_size=0.1)
-    testS, testQ, testA = vectorize(test_data, w2i, max_sentence_size, memory_size)
+    testS, testQ, testA = vectorize(test_data, w2i, max_sentence_size, FLAGS.mem_size)
 
     n_train = trainS.shape[0]
     n_test = testS.shape[0]
