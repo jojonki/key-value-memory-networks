971acdcf0926d525ad88af5d3a8bd5b53cfccc8f
jojonki <junki.ohmura@gmail.com>
2017-10-10 13:29:29 -0400

    refacto

--- train_keras.py
+++ train_keras.py
@@ -1,40 +1,21 @@
 from __future__ import print_function
 
-# from keras import backend as K
-# from keras.models import Sequential, Model
-# from keras.layers.embeddings import Embedding
-# from keras.layers import Input, Activation, Dense, Lambda, Permute, Dropout, add, dot, concatenate
-# from keras.layers import LSTM
-# from keras.utils.data_utils import get_file
-# from keras.preprocessing.sequence import pad_sequences
 from functools import reduce
 from itertools import chain
-# # import tarfile
 import numpy as np
-# import re
-
-import tensorflow as tf
-
 
 from process_data import load_entities, save_pickle, load_pickle, load_kv_pairs, lower_list, vectorize, vectorize_kv_pairs
-from net.memn2n_kv import MemNNKV
-
-# def tokenize(sent):
-#     '''Return the tokens of a sentence including punctuation.
-#     >>> tokenize('Bob dropped the apple. Where is the apple?')
-#     ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
-#     '''
-#     return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
-
+from net.memnn_kv import MemNNKV
 
 is_babi = False
 if is_babi:
-    train_stories = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_train.txt', is_babi)
-    test_stories = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_test.txt', is_babi)
+    train_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_train.txt', is_babi)
+    test_data = load_task('./data/tasks_1-20_v1-2/en/qa5_three-arg-relations_test.txt', is_babi)
 else:
-    N = 10000
-    train_stories = load_pickle('mov_task1_qa_pipe_train.pickle')[:N]
-    test_stories = load_pickle('mov_task1_qa_pipe_test.pickle')[:N]
+    # N = 49900
+    N = 500000000
+    train_data = load_pickle('mov_task1_qa_pipe_train.pickle')[:N]
+    test_data = load_pickle('mov_task1_qa_pipe_test.pickle')[:N]
     kv_pairs = load_pickle('mov_kv_pairs.pickle')
     train_kv_indices = load_pickle('mov_train_kv_indices.pickle')[:N]
     test_kv_indices = load_pickle('mov_test_kv_indices.pickle')[:N]
@@ -48,34 +29,34 @@ else:
     entity_size = len(entities)
 
 vocab = set()
-for story, q, answer in train_stories + test_stories:
+for story, q, answer in train_data + test_data:
     vocab |= set(story + q + answer)
 vocab = sorted(vocab)
 
 # Reserve 0 for masking via pad_sequences
 vocab_size = len(vocab) + 1
-story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
-query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
+story_maxlen = max(map(len, (x for x, _, _ in train_data + test_data)))
+query_maxlen = max(map(len, (x for _, x, _ in train_data + test_data)))
 
 print('-')
 print('Vocab size:', vocab_size, 'unique words')
 print('Story max length:', story_maxlen, 'words')
 print('Query max length:', query_maxlen, 'words')
-print('Number of training stories:', len(train_stories))
-print('Number of test stories:', len(test_stories))
+print('Number of training data:', len(train_data))
+print('Number of test data:', len(test_data))
 print('-')
-print('Here\'s what a "story" tuple looks like (input, query, answer):')
-print(train_stories[0])
+print('Here\'s what a "data" tuple looks like (input, query, answer):')
+print(train_data[0])
 print('-')
 print('Vectorizing the word sequences...')
 
-print('len(entities)', len(entities))
+print('Number of entities', len(entities))
 w2i = dict((c, i + 1) for i, c in enumerate(vocab))
-inputs_train, queries_train, answers_train = vectorize(train_stories,
+inputs_train, queries_train, answers_train = vectorize(train_data,
                                                                w2i,
                                                                story_maxlen,
                                                                query_maxlen, entities)
-inputs_test, queries_test, answers_test = vectorize(test_stories,
+inputs_test, queries_test, answers_test = vectorize(test_data,
                                                             w2i,
                                                             story_maxlen,
                                                             query_maxlen, entities)
@@ -107,7 +88,6 @@ vec_test_kv = vectorize_kv_pairs(test_kv, mem_maxlen, vocab)
 embd_size = 64
 memnn_kv = MemNNKV(mem_maxlen, query_maxlen, vocab_size, entity_size, embd_size)
 print(memnn_kv.summary())
-# train_cands = 
 memnn_kv.fit([vec_train_kv, vec_train_kv, queries_train, answers_train], answers_train,
           batch_size=32,
           epochs=10)#,
