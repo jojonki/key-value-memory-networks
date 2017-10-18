02116f953cdc8bf8516b64757cdb7a8f59e7a382
jojonki <junki.ohmura@gmail.com>
2017-10-18 19:32:53 -0400

    limit overfitting

--- net/memnn_kv.py
+++ net/memnn_kv.py
@@ -20,15 +20,21 @@ def MemNNKV(mem_len, mem_size, query_maxlen, vocab_size, embd_size, answer_size)
     shared_embd_A = Embedding(input_dim=vocab_size, output_dim=embd_size)
 
     key_encoded = shared_embd_A(key) # (None, mem_size, mem_len, embd_size)
+#     key_encoded = Dropout(.3)(key_encoded)
     # print('key_encoded', key_encoded.shape)
     key_encoded = Lambda(lambda x: K.sum(x, axis=2)) (key_encoded) #(None, mem_size, embd_size)
+    key_encoded = BatchNormalization()(key_encoded)
     # print('key_encoded', key_encoded.shape)
     val_encoded = shared_embd_A(val) # (None, mem_size, embd_size)
+#     val_encoded = Dropout(.3)(val_encoded)
     val_encoded = Lambda(lambda x: K.sum(x, axis=2)) (val_encoded)
+    val_encoded = BatchNormalization()(val_encoded)
     
     question_encoded = shared_embd_A(question) # (None, query_max_len, embd_size)
+#     question_encoded = Dropout(.3)(question_encoded)
 #     question_encoded = Lambda(lambda x: K.sum(x, axis=1)) (val_encoded)
     question_encoded = Lambda(lambda x: K.sum(x, axis=1)) (question_encoded) #(None, embd_size)
+    question_encoded = BatchNormalization()(question_encoded)
     # print('q_encoded', question_encoded.shape)
     q= question_encoded
     for h in range(2):
@@ -40,12 +46,16 @@ def MemNNKV(mem_len, mem_size, query_maxlen, vocab_size, embd_size, answer_size)
         # print('o', o.shape)
         o = Lambda(lambda x: K.sum(x, axis=2))(o) # (None, embd_size)
         # print('o', o.shape)
+#         R = Dense(embd_size, input_shape=(embd_size,), name='R_Dense_h' + str(h+1), kernel_regularizer=regularizers.l2(0.01))
         R = Dense(embd_size, input_shape=(embd_size,), name='R_Dense_h' + str(h+1))
         q = R(add([q,  o])) # (None, embd_size)
+        q = BatchNormalization()(q)
         # print('q', q.shape)
 
 #     answer = Dense(answer_size, name='last_Dense')(q) #(None, answer_size)
+#     answer = Dense(vocab_size, name='last_Dense', kernel_regularizer=regularizers.l2(0.01))(q) #(None, vocab_size)
     answer = Dense(vocab_size, name='last_Dense')(q) #(None, vocab_size)
+    answer = BatchNormalization()(answer)
     # print('answer.shape', answer.shape)
     preds = Activation('softmax')(answer)
     
@@ -53,3 +63,4 @@ def MemNNKV(mem_len, mem_size, query_maxlen, vocab_size, embd_size, answer_size)
     model = Model([key, val, question], preds)
     model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=['accuracy']) 
     return model
+
